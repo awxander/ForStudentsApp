adc8bdd0f5c12fd1945b770c14604f503bb1dcd0
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-04-01 18:09:00 +0700

    реализована LoginActivity.kt

--- forstudents/src/main/java/com/example/forstudents/LoginActivity.kt
+++ forstudents/src/main/java/com/example/forstudents/LoginActivity.kt
@@ -1,16 +1,20 @@
 package com.example.forstudents
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.forstudents.data.model.UserLoginModel
 import com.example.forstudents.databinding.ActivityLoginBinding
+import com.example.forstudents.ui.FormState
 import com.example.forstudents.ui.LoginRegisterViewModel
 import com.example.forstudents.ui.LoginRegisterViewModelFactory
 import com.example.forstudents.ui.RegisterLoginState
+import com.example.forstudents.util.afterTextChanged
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var loginRegisterViewModel: LoginRegisterViewModel
 
 
@@ -23,16 +27,76 @@ class LoginActivity : AppCompatActivity() {
         )[LoginRegisterViewModel::class.java]
         setContentView(binding.root)
         setObservers()
+        setListeners()
     }
 
-    private fun setObservers(){
-        loginRegisterViewModel.state.observe(this@LoginActivity, ::handleRegisterLoginState)
+    private fun setListeners() {
+        binding.apply {
+            signInBtn.setOnClickListener {
+                loginRegisterViewModel.login(
+                    UserLoginModel(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    )
+                )
+            }
+
+            emailEditText.afterTextChanged {
+                loginRegisterViewModel.loginDataChanged(
+                    emailEditText.text.toString(),
+                    passwordEditText.text.toString(),
+                )
+            }
+
+            passwordEditText.apply {
+                afterTextChanged {
+                    loginRegisterViewModel.loginDataChanged(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    )
+                }
+            }
+
+        }
     }
 
-    private fun handleRegisterLoginState(state : RegisterLoginState){
-
+    private fun setObservers() {
+        loginRegisterViewModel.registerLoginState.observe(
+            this@LoginActivity,
+            ::handleLoginState
+        )
+        loginRegisterViewModel.formState.observe(this@LoginActivity, ::handleFormState)
     }
 
+    private fun handleLoginState(state: RegisterLoginState) {
+        when (state) {
+            RegisterLoginState.Initial -> Unit
+            RegisterLoginState.Loading -> Unit //add progress bar
+            is RegisterLoginState.Content -> Unit
+            is RegisterLoginState.Error -> showErrorMsg(state.text)
+        }
+    }
+
+    private fun showErrorMsg(errorMsg: String) {
+        binding.tvErrorMsg.apply {
+            text = errorMsg
+            visibility = View.VISIBLE
+        }
+    }
+
+    private fun handleFormState(state: FormState) {
+        binding.apply {
+
+            signInBtn.isEnabled = state.isDataValid
+
+            if (state.emailError != null) {
+                emailEditText.error = getString(state.emailError)
+            }
+            if (state.passwordError != null) {
+                passwordEditText.error = getString(state.passwordError)
+            }
+        }
+    }
 
 
 }
