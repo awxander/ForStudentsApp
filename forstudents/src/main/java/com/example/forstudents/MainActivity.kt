a79365bba58e3de010fdef0fdc3311ba458881ea
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-04-05 18:33:00 +0700

    добавлен bottomNavigation

--- forstudents/src/main/java/com/example/forstudents/MainActivity.kt
+++ forstudents/src/main/java/com/example/forstudents/MainActivity.kt
@@ -2,15 +2,49 @@ package com.example.forstudents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.forstudents.databinding.ActivityMainBinding
+import com.example.forstudents.fragments.MessagesFragment
+import com.example.forstudents.fragments.ProfileFragment
+import com.example.forstudents.fragments.QuestionsFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private val profileFragment = ProfileFragment()
+    private val messagesFragment = MessagesFragment()
+    private val questionsFragment = QuestionsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
+        setContentView(binding.root)
+        setBottomNav()
     }
+
+    private fun setBottomNav() {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.profile -> {
+                    setCurrentFragment(profileFragment)
+                }
+                R.id.messages -> {
+                    setCurrentFragment(messagesFragment)
+                }
+                R.id.questions -> {
+                    setCurrentFragment(questionsFragment)
+                }
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView, fragment)
+            commit()
+        }
+
+
 }
