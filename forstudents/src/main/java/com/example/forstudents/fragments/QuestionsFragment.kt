fe2bd74574bcb6bec48a04ba693424fc3e2f372d
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-04-07 14:39:00 +0700

    начальное заполенение QuestionsFragment.kt

--- forstudents/src/main/java/com/example/forstudents/fragments/QuestionsFragment.kt
+++ forstudents/src/main/java/com/example/forstudents/fragments/QuestionsFragment.kt
@@ -5,12 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.forstudents.MainActivity
 import com.example.forstudents.R
 import com.example.forstudents.databinding.FragmentQuestionsBinding
+import com.example.forstudents.util.hideBottomNavigation
+import com.example.forstudents.util.showBottomNavigation
 
 class QuestionsFragment : Fragment() {
 
-    private lateinit var binding : FragmentQuestionsBinding
+    private lateinit var binding: FragmentQuestionsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showBottomNavigation()
+        setListeners()
+    }
+
+    private fun setListeners(){
+        binding.createQuestionButton.setOnClickListener {
+            startNewQuestionFragment()
+            hideBottomNavigation()
+        }
+    }
+
+
+    private fun startNewQuestionFragment(){
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, NewQuestionFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +47,6 @@ class QuestionsFragment : Fragment() {
         binding = FragmentQuestionsBinding.inflate(inflater)
         return binding.root
     }
+
 
 }
