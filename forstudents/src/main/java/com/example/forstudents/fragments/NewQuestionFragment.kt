12334af87be48bdf9678f3c95ef9afa8680563c3
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-04-12 13:03:36 +0700

    добавлены/удалены комментарии

--- forstudents/src/main/java/com/example/forstudents/fragments/NewQuestionFragment.kt
+++ forstudents/src/main/java/com/example/forstudents/fragments/NewQuestionFragment.kt
@@ -5,18 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.forstudents.databinding.FragmentNewQuestionBinding
 
 class NewQuestionFragment : Fragment() {
 
-
     private lateinit var binding: FragmentNewQuestionBinding
+    private val navController get() = findNavController()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View{
         binding = FragmentNewQuestionBinding.inflate(inflater)
+        //TODO пофиксить хуйню с
+        // закрытием приложения на back
         return binding.root
     }
 
